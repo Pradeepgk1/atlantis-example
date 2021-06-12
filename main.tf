d5466a2f21c77b37326f9a748efe52be4b60730e
Pradeepgk1 <43160875+Pradeepgk1@users.noreply.github.com>
2021-06-12 14:49:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
 resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-047a51fa27710816e"
+  ami               = " ami-0aeeebd8d2ab47354"
   instance_type     = "t2.micro"
   #availability_zone = "us-east-1a"
   availability_zone = "us-east-1b"
